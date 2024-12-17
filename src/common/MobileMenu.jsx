094c5d5a7e70e8513f9c76cd45b62633c82ba41c
necati1054 <necati.arman1054@gmail.com>
2024-12-17 13:19:24 +0300

    menu login button add

--- src/common/MobileMenu.jsx
+++ src/common/MobileMenu.jsx
@@ -1,50 +1,69 @@
-import React, { useContext } from 'react';
-import Logo from './Logo';
-import NavMenu from './NavMenu';
-import { MobileMenuContext } from '../contextApi/MobileMenuContext';
-import { ScrollHideContext } from '../contextApi/ScrollHideContext';
-import Button from './Button';
+import React, { useContext } from "react";
+import Logo from "./Logo";
+import NavMenu from "./NavMenu";
+import { MobileMenuContext } from "../contextApi/MobileMenuContext";
+import { ScrollHideContext } from "../contextApi/ScrollHideContext";
+import Button from "./Button";
+import { useTranslation } from "react-i18next";
 
 const MobileMenu = () => {
+  const { toggleMobileMenu, handleMobileMenuClose } =
+    useContext(MobileMenuContext);
 
-    const { toggleMobileMenu, handleMobileMenuClose } = useContext(MobileMenuContext);
+  const { handleScrollHideClose } = useContext(ScrollHideContext);
 
-    const { handleScrollHideClose } = useContext(ScrollHideContext); 
-    
-    return (
-        <>
-            {/* ==================== Mobile Menu Start Here ==================== */}
-            <div className={`side-overlay ${toggleMobileMenu ? 'show' : "" }`} onClick={()=> {handleMobileMenuClose(); handleScrollHideClose(); }}></div>
-            
-            <div className={`mobile-menu d-lg-none d-block ${toggleMobileMenu ? 'active' : "" }`}>
-                <button type="button" className="close-button" onClick={ ()=> {handleMobileMenuClose(); handleScrollHideClose(); }}>
-                    <i className="las la-times"></i> 
-                </button>
-                <div className="mobile-menu__inner">
+  const { t, i18n } = useTranslation();
 
-                    {/* Logo */}
-                    <Logo/>
-                    
-                    <div className="mobile-menu__menu">
+  let lang = i18n.resolvedLanguage;
 
-                        {/* Nav Menu */}
-                        <NavMenu navMenusClass="nav-menu--mobile" />
+  return (
+    <>
+      {/* ==================== Mobile Menu Start Here ==================== */}
+      <div
+        className={`side-overlay ${toggleMobileMenu ? "show" : ""}`}
+        onClick={() => {
+          handleMobileMenuClose();
+          handleScrollHideClose();
+        }}
+      ></div>
 
-                        {/* Header Button */}
-                        <Button
-                            btnLink="/property" 
-                            btnClass="btn-outline-light d-lg-none d-block mt-4" 
-                            btnText="Learn More" 
-                            spanClass="icon-right text-gradient" 
-                            iconClass="fas fa-arrow-right" 
-                        />
-                        
-                    </div>
-                </div>
-            </div>
-            {/* ==================== Mobile Menu End Here ==================== */}
-        </>
-    );
+      <div
+        className={`mobile-menu d-lg-none d-block ${
+          toggleMobileMenu ? "active" : ""
+        }`}
+      >
+        <button
+          type="button"
+          className="close-button"
+          onClick={() => {
+            handleMobileMenuClose();
+            handleScrollHideClose();
+          }}
+        >
+          <i className="las la-times"></i>
+        </button>
+        <div className="mobile-menu__inner">
+          {/* Logo */}
+          <Logo />
+
+          <div className="mobile-menu__menu">
+            {/* Nav Menu */}
+            <NavMenu navMenusClass="nav-menu--mobile" />
+
+            {/* Header Button */}
+            <Button
+              btnLink={"/" + lang + "/login"}
+              btnClass="btn-outline-light d-lg-none d-block mt-4"
+              btnText={t("Giriş Yap")}
+              spanClass="icon-right text-gradient"
+              iconClass="fas fa-arrow-right"
+            />
+          </div>
+        </div>
+      </div>
+      {/* ==================== Mobile Menu End Here ==================== */}
+    </>
+  );
 };
 
 export default MobileMenu;
--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,75 +1,88 @@
-import React from 'react';
-import { aboutContent, aboutStatistics } from '../data/HomeOneData/HomeOneData';
-import Button from '../common/Button';
-import SectionHeading from '../common/SectionHeading';
+import React from "react";
+import { aboutContent, aboutStatistics } from "../data/HomeOneData/HomeOneData";
+import Button from "../common/Button";
+import SectionHeading from "../common/SectionHeading";
 
-import CountUp from 'react-countup';
+import CountUp from "react-countup";
+import { useTranslation } from "react-i18next";
 
 const About = () => {
-    return (
-        <>
-            {/* ======================== About Section Start ========================== */}
-            <section className="about padding-y-120">
-                <div className="container container-two">
-                    <div className="row gy-4 align-items-center">
-                        <div className="col-lg-6">
-                            <div className="about-thumb">
-                                <img src={aboutContent.thumb} alt=""/>
-                                <div className="client-statistics flx-align">
-                                    <span className="client-statistics__icon">
-                                        {aboutStatistics.icon}
-                                    </span>
-                                    <div className="client-statistics__content">
-                                        <h5 className="client-statistics__number statisticsCounter">
-                                            <CountUp end={parseInt(aboutStatistics.number)}/>
-                                        </h5>
-                                        <span className="client-statistics__text fs-18">{aboutStatistics.text}</span>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col-lg-6">
-                            <div className="about-content">
+  const { t, i18n } = useTranslation();
 
-                                <SectionHeading 
-                                    headingClass="style-left"  
-                                    subtitle="About Us"
-                                    subtitleClass="" 
-                                    title="Stay with us feel at home Your perfect stay awaits" 
-                                    renderDesc={false}
-                                    desc=""
-                                    renderButton={false}
-                                    buttonClass="btn-main"
-                                    buttonText="View More"
-                                />
-                                
-                                <div className="about-box d-flex">
-                                    <div className="about-box__icon">
-                                        <img src={aboutContent.icon} alt="" />
-                                    </div>
-                                    <div className="about-box__content">
-                                        <h6 className="about-box__title">{aboutContent.title}</h6>
-                                        <p className="about-box__desc font-13">{aboutContent.desc}</p>
-                                    </div>
-                                </div>
-                                <div className="about-button">
-                                    <Button 
-                                        btnLink="/about-us" 
-                                        btnClass="btn-main" 
-                                        btnText="Learn More" 
-                                        spanClass="icon-right" 
-                                        iconClass="fas fa-arrow-right" 
-                                    />
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+  let lang = i18n.resolvedLanguage;
+  return (
+    <>
+      {/* ======================== About Section Start ========================== */}
+      <section className="about padding-y-120">
+        <div className="container container-two">
+          <div className="row gy-4 align-items-center">
+            <div className="col-lg-6">
+              <div className="about-thumb">
+                <img src={aboutContent.thumb} alt="" />
+                <div className="client-statistics flx-align">
+                  <span className="client-statistics__icon">
+                    {aboutStatistics.icon}
+                  </span>
+                  <div className="client-statistics__content">
+                    <h5 className="client-statistics__number statisticsCounter">
+                      <CountUp end={parseInt(aboutStatistics.number)} />
+                    </h5>
+                    <span className="client-statistics__text fs-18">
+                      {aboutStatistics.text}
+                    </span>
+                  </div>
                 </div>
-            </section>
-            {/* ======================== About Section End ========================== */}
-   
-        </>
-    );
+              </div>
+            </div>
+            <div className="col-lg-6">
+              <div className="about-content">
+                <SectionHeading
+                  headingClass="style-left"
+                  subtitle="About Us"
+                  subtitleClass=""
+                  title="Stay with us feel at home Your perfect stay awaits"
+                  renderDesc={false}
+                  desc=""
+                  renderButton={false}
+                  buttonClass="btn-main"
+                  buttonText="View More"
+                />
+
+                <div className="about-box d-flex">
+                  <div className="about-box__icon">
+                    <img src={aboutContent.icon} alt="" />
+                  </div>
+                  <div className="about-box__content">
+                    <h6 className="about-box__title">{aboutContent.title}</h6>
+                    <p className="about-box__desc font-13">
+                      {aboutContent.desc}
+                    </p>
+                  </div>
+                </div>
+                <div className="about-button">
+                  <Button
+                    btnLink="/about-us"
+                    btnClass="btn-main"
+                    btnText="Learn More"
+                    spanClass="icon-right"
+                    iconClass="fas fa-arrow-right"
+                  />
+                  <Button
+                    btnLink={"/" + lang + "/login"}
+                    btnClass="btn btn-outline-light d-lg-block d-none"
+                    btnText={t("Giriş Yap")}
+                    spanClass="icon-right text-gradient"
+                    iconClass="fas fa-arrow-right"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      {/* ======================== About Section End ========================== */}
+    </>
+  );
 };
 
 export default About;
